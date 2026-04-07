740cd4b5a8288563d1078201445a740fa053099c
Alexandr-Klim <klimenkoaa@list.ru>
2026-04-07 19:57:07 +1000

    Create  TwoSum.java

---
+++ src/hashmap/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    // Поиск индексов двух чисел с суммой target
+    // Сложность: O(n)
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+}
