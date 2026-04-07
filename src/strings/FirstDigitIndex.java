a6c01e1a547c0aaf54c33546c852a8d0108b4d39
Alexandr-Klim <klimenkoaa@list.ru>
2026-04-07 19:51:48 +1000

    Update FirstDigitIndex.java

--- src/strings/FirstDigitIndex.java
+++ src/strings/FirstDigitIndex.java
@@ -1,7 +1,12 @@
 public class FirstDigitIndex {
-    public static int indexOfFirstDigit(String text){
-        for(int i = 0; i < text.length(); i++){
-            if(Character.isDigit(text.charAt(i))){
+
+    // Поиск индекса первой цифры
+    // Сложность: O(n)
+    public static int indexOfFirstDigit(String text) {
+        if (text == null || text.isEmpty()) return -1;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i))) {
                 return i;
             }
         }
