3c252503bd6fa77add2144b823cdd529e13e681a
Alexandr-Klim <klimenkoaa@list.ru>
2026-04-07 19:47:43 +1000

    Update FirstDigitIndex.java

--- src/strings/FirstDigitIndex.java
+++ src/strings/FirstDigitIndex.java
@@ -1,1 +1,10 @@
-
+public class FirstDigitIndex {
+    public static int indexOfFirstDigit(String text){
+        for(int i = 0; i < text.length(); i++){
+            if(Character.isDigit(text.charAt(i))){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
